e1392dd82b468096c8127c8f682844a4814125b2
Billy <billyn0213@gmail.com>
2022-08-28 12:19:53 -0400

    pushing

---
+++ server.js
@@ -0,0 +1,11 @@
+const MONGODB_URI  = process.env.MONGODB_URI 
+
+// Connect to Mongo
+mongoose.connect(MONGODB_URI, () => {
+    console.log('whatever')
+})
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
